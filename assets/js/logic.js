91ccb2a26af94c95ca099b84169c1443e669ab92
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-01-15 18:41:36 +0000

    Added questions and numerous functions for flow throughout the quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,26 +6,75 @@ var questionWrapper = document.getElementById("questions");
 var questionTitle = document.getElementById("question-title");
 var questionChoices = document.getElementById("choices");
 var secondsLeft = 60;
+var questionNumber = 0
+let timerInterval;
+var finalScore = 0;
 
 startButton.addEventListener("click", function() {
 
     setTimer();
+        startScreen.classList.add("hide");
+        questionWrapper.classList.remove("hide");
+        addQuestionTitle();
+        
+        // Function to change question title and add choices
 
         // Hide Start Screen state
         // Show questionwrapper state
-        //revise event.target?
-        
-        
+        // revise event.target?             
 })
 
+function addQuestionTitle() {
+    
+    if(quizQuestions.length === questionNumber) {
+        return endGame();
+    }
+    questionTitle.textContent = quizQuestions[questionNumber].question;
+    questionChoices.innerHTML = "";
+
+    for(var i = 0; i < quizQuestions[questionNumber].answers.length; i++) {
+        var btn = document.createElement("button");
+        btn.textContent = quizQuestions[questionNumber].answers[i];
+        questionChoices.append(btn);
+    }
+
+}
+
+function endGame() {
+    finalScore = secondsLeft;
+    clearInterval(timerInterval);
+    
+    // add End screen
+    // Get initials of player and store with high score in scores.js
+    
+}
+
+questionChoices.addEventListener("click", questionClickHandler)
+
+function questionClickHandler(event) {
+   var userAnswer = event.target.textContent;    
+
+   if (userAnswer === quizQuestions[questionNumber].correct) {
+    // Notify user is correct
+    questionNumber++;
+    addQuestionTitle();
+   }
+   else { 
+    secondsLeft -= 5;
+    questionNumber++;
+    addQuestionTitle();
+   }
+}
+
+// event is referring to the function event (addEventListener - click)
 function setTimer() {
-    let timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
         secondsLeft--;
         timer.textContent = secondsLeft + " seconds left";
 
         if(secondsLeft === 0) {
             //TODO Function upon timer expires
-            clearInterval(timerInterval);
+            endGame();
             timer.textContent = 0;
             alert("Times up!")
         }
@@ -37,15 +86,14 @@ function incorrectAnswer() {
     timer.textContent = secondsLeft + " seconds left";
 }
 
-
-
-
 // NOTES
 
 //setInterval() calls a function at specified intervals
 // an anonymous function is a function without a name. It needs to be assigned to a variable. You often pass anonymous functions as arguments to other functions
 
-
-
-
+/* To keep high scores
+localStorage.setItem(key, value);
+localStorage.setItem("highscore", "var userScore");
+var myNumber = localStorage.getItem ("highscore");
+*/
 
---
+++ assets/js/questions.js
@@ -0,0 +1,47 @@
+// Array of Objects 
+
+var quizQuestions = [
+     {
+        question: "Which one of these is NOT a valid data type in JavaScript?",
+        answers: ["String", "Boolean", "Float", "Number"],
+        correct: "Float"
+    },
+
+     {
+        question: "Question2",
+        answers: ["answerOne", "answerTwo", "answerThree", "answerFour"],
+        correct: "answerOne"
+    },
+
+     {
+        question: "Question3",
+        answers: ["answerOne", "answerTwo", "answerThree", "answerFour"],
+        correct: "answerOne"
+    },
+
+     {
+        question: "Question4",
+        answers: ["answerOne", "answerTwo", "answerThree", "answerFour"],
+        correct: "answerOne"
+    },
+
+     {
+        question: "Question5",
+        answers: ["answerOne", "answerTwo", "answerThree", "answerFour"],
+        correct: "answerOne"
+    },
+
+]
+
+var correctAnswers = ["2", "1", "1", "1", "1"];
+
+if (quizQuestions[0].answers[0] === "String") {
+    console.log("True")
+}
+
+
+
+
+// 4 buttons for each question. 
+
+
